45dad5c229b7d1433df8b5b6ba292a584a708c25
Seagyn Davis <seagyn@flickerleap.com>
2019-01-05 01:02:12 +0200

    Finalise ability to set primary category

--- js/wordpress-primary-categories.js
+++ js/wordpress-primary-categories.js
@@ -1,14 +1,76 @@
 jQuery( function ( $ ) {
-	$( '#categorychecklist' ).find( 'input' ).on( 'change', function( event ) {
-		var categoryId = $(event.currentTarget).val();
-		var isPrimary = false;
+	var postId = parseInt( $( '#post_ID' ).val() );
 
-		if ( !isPrimary ) {
-			togglePrimarySelection( event.currentTarget );
-		}
-	} );
+	/*
+	 * We cannot do anything until we have a post ID.
+	 * We will wait for that to happen (save draft or publish).
+	 */
+	if ( 'number' === typeof postId ) {
+		$( '#categorychecklist' ).find( 'input:checked' ).each( function ( index, el ) {
+			var category = {
+				'id': $(el).val(),
+				'name': $(el).siblings('label').text()
+			};
 
-	$
+			if ( category.id !== wpc_data.primary_category_id ) {
+				$( el ).parents('li').append('<a href="#" class="wpc-primary-selector" title="' + wpc_data.link_title + '" data-category-id="' + category.id + '">' + wpc_data.label + '</a>'); // TODO: Add category name in title.
+			}
+		} );
+
+		$( '#categorychecklist' ).find( 'input' ).on( 'change', function( e ) {
+			var category = {
+				'id': $(e.currentTarget).val(),
+				'name': $(e.currentTarget).siblings('label').text()
+			};
+			var isPrimary = false;
+
+			if ( category.id !== wpc_data.primary_category_id ) {
+				togglePrimarySelection( e.currentTarget, category );
+			}
+		} );
+
+		$( '#categorychecklist' ).on( 'click', '.wpc-primary-selector', function( e ) {
+			e.preventDefault();
+
+			var el = $(this);
+
+			el.fadeOut();
+
+			var postData = {
+				'action': 'set_primary_category',
+				'category_id': $(this).data('category-id'),
+				'nonce': wpc_data.nonce,
+				'old_category_id': wpc_data.primary_category_id, // passing the current id to keep post_meta entries nice and small
+				'post_id': postId
+			};
+
+			/*
+			 * Do the AJAX request to update the primary category.
+			 *
+			 * On failure, flash a message and add the link again.
+			 * On success, add a link to the old category (if there was one) and then remove the link on the new category.
+			 */
+			$.post( ajaxurl, postData).fail( function ( jqXHR ) {
+				el.before('<span class="wpc-error">' + jqXHR.responseJSON.data + '</span>');
+				$('.wpc-error').fadeOut( 2000, function () {
+					el.fadeIn();
+					$('.wpc-error').remove();
+				} );
+			} ).done( function () {
+				if ( wpc_data.primary_category_id !== '' ) {
+					var old_primary_element =  $( '#in-category-' + wpc_data.primary_category_id );
+					var category = {
+						'id': wpc_data.primary_category_id,
+						'name': $(old_primary_element).siblings('label').text()
+					};
+
+					$( old_primary_element ).parents('li').append('<a href="#" class="wpc-primary-selector" title="' + wpc_data.link_title + '" data-category-id="' + category.id + '">' + wpc_data.label + '</a>');
+				}
+				wpc_data.primary_category_id = el.data('category-id');
+				el.remove();
+			});
+		});
+	}
 } );
 
 /**
@@ -17,12 +79,12 @@ jQuery( function ( $ ) {
  * @since      0.1
  *
  * @param {HTMLInputElement} target Input element that was checked / unchecked.
- *
+ * @param {object}           category Category that is being selected.
  * @return void
  */
-function togglePrimarySelection ( target ) {
+function togglePrimarySelection ( target, category ) {
 	var checked = $(target).attr('checked');
-	var el = $(target).parent();
+	var el = $(target).parents('li');
 
 	/*
 	 * If we are now unchecked, let's remove the link.
@@ -31,6 +93,6 @@ function togglePrimarySelection ( target ) {
 	if ( ! checked ) {
 		el.find('.wpc-primary-selector').remove();
 	} else {
-		el.append('<a href="#" class="wpc-primary-selector">' + wpc_data.label + '</a>');
+		el.append('<a href="#" class="wpc-primary-selector" title="' + wpc_data.link_title + '" data-category-id="' + category.id + '">' + wpc_data.label + '</a>'); // TODO: Add category name in title.
 	}
 }
--- wordpress-primary-categories.php
+++ wordpress-primary-categories.php
@@ -1,29 +1,33 @@
 <?php
-/*
-Plugin Name:  WordPress Primary Categories
-Description:  Allows the user to select a primary category for a post.
-Version:      0.1
-Author:       Seagyn Davis
-Author URI:   https://www.seagyndavis.com/
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wordpress-primary-categories
-Requires PHP: 5.4
-*/
+/**
+ * Plugin Name:  WordPress Primary Categories
+ * Description:  Allows the user to select a primary category for a post.
+ * Version:      0.1
+ * Author:       Seagyn Davis
+ * Author URI:   https://www.seagyndavis.com/
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  wordpress-primary-categories
+ * Requires PHP: 5.6
+ *
+ * @package WPC
+ */
 
 namespace SeagynDavis\WPC;
 
 define( 'WPC_VERSION', '0.1' );
 
 /**
- * We are adding our script to the admin post add / edit pages.
+ * Adding our script to the admin post add / edit pages.
  *
  * @param string $hook The current admin page.
  */
 function admin_enqueue_scripts( $hook ) {
-	if ( 'edit.php' !== $hook && 'post-new.php' !== $hook ) {
+	if ( ! in_array( $hook, [ 'edit.php', 'post.php' ], true ) ) {
 		return;
 	}
+
+	global $post;
 
 	wp_enqueue_style(
 		'wordpress-primary-categories',
@@ -39,13 +43,84 @@ function admin_enqueue_scripts( $hook ) {
 		WPC_VERSION,
 		true
 	);
+
+	$primary_category_id = get_post_meta( $post->ID, '_primary_category_id', true );
+
+	$localized_data = [
+		'label'               => __( 'Make Primary', 'wordpress-primary-categories' ),
+		'link_title'          => __( 'Set as the primary category.', 'wordpress-primary-categories' ),
+		'nonce'               => wp_create_nonce( 'wpc-nonce' ),
+		'primary_category_id' => $primary_category_id,
+	];
 	wp_localize_script(
 		'wordpress-primary-categories',
 		'wpc_data',
-		[
-			'label' => __( 'Make Primary', 'wordpress-primary-categories' ),
-		]
+		$localized_data
 	);
 }
 add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\admin_enqueue_scripts' );
 
+/**
+ * Handles the admin ajax request.
+ *
+ * @return void
+ */
+function set_primary_category() {
+	/*
+	 * Checking to see if the nonce was set and is correct. If you're sending an AJAX request, make sure you set the nonce in form data.
+	 */
+	if ( ! check_ajax_referer( 'wpc-nonce', 'nonce' ) ) {
+		wp_send_json_error(
+			__( 'Invalid security token sent.', 'wpc-primary-categories' ),
+			'403'
+		); // Send Forbidden response.
+		wp_die();
+	}
+
+	if ( ! current_user_can( 'edit_posts' ) ) {
+		wp_send_json_error( __( 'Unauthorised.', 'wpc-primary-categories' ), '401' ); // Send Unauthorized response.
+		wp_die();
+	}
+
+	$category_id     = null;
+	$post_id         = null;
+	$old_category_id = null;
+
+	/*
+	 * Checking to see if the nonce was set and is correct. If you're sending an AJAX request, make sure you set the nonce in form data.
+	 */
+	if ( isset( $_POST['category_id'] ) && isset( $_POST['post_id'] ) && isset( $_POST['old_category_id'] ) ) {
+		$category_id     = intval( $_POST['category_id'] );
+		$post_id         = intval( $_POST['post_id'] );
+		$old_category_id = intval( $_POST['old_category_id'] );
+	} else {
+		wp_send_json_error(
+			__( 'Missing parameters from request.', 'wpc-primary-categories' ),
+			'422'
+		); // Send Unprocessable Entity response.
+		wp_die();
+	}
+
+	/*
+	 * Checking to see if the nonce was set and is correct. If you're sending an AJAX request, make sure you set the nonce in form data.
+	 */
+	if ( ! $category_id || ! $post_id ) {
+		wp_send_json_error(
+			__( 'Invalid parameters passed in request.', 'wpc-primary-categories' ),
+			'422'
+		); // Send Unprocessable Entity response.
+		wp_die();
+	}
+
+	if ( update_post_meta( $post_id, '_primary_category_id', $category_id, $old_category_id ) ) {
+		wp_send_json_success( __( 'Primary category updated.', 'wpc-primary-categories' ) );
+	} else {
+		wp_send_json_error(
+			__( 'Could not update primary category.', 'wpc-primary-categories' ),
+			'500'
+		); // Send Internal Server Error response.
+	}
+
+	wp_die();
+}
+add_action( 'wp_ajax_set_primary_category', __NAMESPACE__ . '\set_primary_category' );
